a47839c5fd635206ac4daba92338ecd437613908
finny <phineas.ehrlich2026@gbwl.org>
2024-12-06 14:40:46 +0000

    persona

---
+++ Student.java
@@ -0,0 +1,17 @@
+/* Example CLASS that defines an OBJECT
+Class names like student are always capitalized
+Object-Oriented Classes are like BLUEPRINTS
+(in another Java class, you create instances from this blueprint)
+*/
+public class Person {
+    // 1. Instance Variables (attributes/data)
+    // think of waht this object needs to know
+    String name;
+    int gradYear;
+    
+
+    // 2. Constructors (manuals for setting up an object)
+
+    
+    // 3. Methods (behaviors associated with the object type)
+ }
