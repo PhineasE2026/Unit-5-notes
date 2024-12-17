cbd51607384eb38ca60a94aafae9521e469400fc
finny <phineas.ehrlich2026@gbwl.org>
2024-12-17 13:52:01 +0000

    dime

--- Main.java
+++ Main.java
@@ -2,6 +2,35 @@
 public class Main {
 
    public static void main(String []args) {
+      Student kinder = new Student();
+      Student junior = new Student("James", "Bumble", 2037);
+      System.out.println(junior);
+
+      String juniorEmail = junior.getEmail();
+      System.out.println(juniorEmail);
+      System.out.println(junior.getEmail());
+
+      System.out.println(kinder.getGradYear());
+      kinder.setGradYear(2039);
+      System.out.println(kinder.getGradYear());
+
+      System.out.println(junior.getFirstName());
+      junior.setFirstName("Jimothy");
+      System.out.println(junior.getFirstName());
+
+      kinder.setFirstName("Aluper");
+      kinder.setLastName("Repula");
+      System.out.println(kinder);
+
+      kinder.setEmail("Aluper.Repula2039@gbwl.org");
+      System.out.println(kinder);
+
+      //value of each instance variable has to be checked. each instance variable has to be checked.
+      // junior is the arg, kinder is the obj
+      boolean isEqual = kinder.equals(junior);
+      System.out.println(isEqual);
       
+      var brimble = 3 + 5 + "hello bryce" + 2.5;
+      System.out.println(brimble);
    }
 }
--- Student.java
+++ Student.java
@@ -3,15 +3,66 @@ Class names like student are always capitalized
 Object-Oriented Classes are like BLUEPRINTS
 (in another Java class, you create instances from this blueprint)
 */
-public class Person {
+public class Student {
     // 1. Instance Variables (attributes/data)
     // think of waht this object needs to know
-    String name;
-    int gradYear;
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private String email;
     
 
     // 2. Constructors (manuals for setting up an object)
+    public Student() {
+        firstName = "First";
+        lastName = "Last";
+        gradYear = 2037;
+        email = firstName + "." + lastName + gradYear + "@gbwl.org";
+    }
 
+    public Student(String initFirstName, String initLastName, int initGradYear) {
+        firstName = initFirstName;
+        lastName = initLastName;
+        gradYear = initGradYear;
+        email = firstName + "." + lastName + gradYear + "@gbwl.org";
+    }
     
     // 3. Methods (behaviors associated with the object type)
+    public String toString() {
+        return ("Student [" + firstName + "," + lastName + "," + gradYear + "," + email + "]");
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getGradYear() {
+        return gradYear;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // setters
+
+    public void setFirstName(String newFirstName) {
+        firstName = newFirstName;
+    }
+
+    public void setLastName(String newLastName) {
+        lastName = newLastName;
+    }
+
+    public void setGradYear(int newGradYear) {
+        gradYear = newGradYear;
+    }
+
+    public void setEmail(String newEmail) {
+        email = newEmail;
+    }
  }
