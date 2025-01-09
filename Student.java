dfb45cfa72383644aaa88c77d954fc196a2a733b
finny <phineas.ehrlich2026@gbwl.org>
2025-01-09 13:36:26 +0000

    ADDED THIS. THIS. THIS. THIS. THIS.

--- Student.java
+++ Student.java
@@ -17,18 +17,21 @@ public class Student {
 
     // 2. Constructors (manuals for setting up an object)
     public Student() {
-        firstName = "First";
-        lastName = "Last";
-        gradYear = 2037;
-        email = firstName + "." + lastName + gradYear + "@gbwl.org";
+        this.firstName = "First";
+        this.lastName = "Last";
+        this.gradYear = 2037;
+        this.email = firstName + "." + lastName + gradYear + "@gbwl.org";
+        // increment the student object counter static variable
+        // do not use this.studentCount because it is data belonging to the class not an object instance...
         studentCount++;
     }
 
-    public Student(String initFirstName, String initLastName, int initGradYear) {
-        firstName = initFirstName;
-        lastName = initLastName;
-        gradYear = initGradYear;
-        email = firstName + "." + lastName + gradYear + "@gbwl.org";
+    public Student(String firstName, String lastName, int gradYear) {
+        // THIS. THIS. THIS. THIS. THISkeyword indicates THIS.current callling object.
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
+        this.email = firstName + "." + lastName + gradYear + "@gbwl.org";
         studentCount++;
     }
     
